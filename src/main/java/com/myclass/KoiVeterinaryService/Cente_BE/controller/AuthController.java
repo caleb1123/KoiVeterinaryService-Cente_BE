2376624657a32f42dc5a1ffe0ac4300b460be25c
Caleb <105915564+caleb1123@users.noreply.github.com>
2024-09-27 23:19:18 +0700

    Update AuthController.java

--- src/main/java/com/myclass/KoiVeterinaryService/Cente_BE/controller/AuthController.java
+++ src/main/java/com/myclass/KoiVeterinaryService/Cente_BE/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @Slf4j
-@CrossOrigin(origins = "http://localhost:3001")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
     @Autowired
     AuthService authService;
